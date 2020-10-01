b748a2b471238412011ba2089b836d42ee0014ac
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2020-10-02 00:30:06 +0200

    Tests for subgradient.
    Fix an issue when the initial solution is infeasible: it could have been wrongly seen as the best iterate.

--- src/subgradient.jl
+++ src/subgradient.jl
@@ -20,10 +20,10 @@ function _solve_sd(p::NonSmoothProblem, params::NonSmoothSolver, x0::Vector{Floa
   # Assumption for p' and params' types: either (UnconstrainedNonSmoothProblem, SubgradientMethod) or (ProjectedConstrainedNonSmoothProblem, ProjectedSubgradientMethod).
   @assert length(x0) == p.dimension
 
-  x_best = copy(x0)
   if p isa ProjectedConstrainedNonSmoothProblem
     x0 = p.project(x0)
   end
+  x_best = copy(x0)
   f_best = p.f(x0)
 
   dir = ((p.sense == Minimise) ? -1.0 : 1.0)::Float64
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
   @testset "Subgradient" begin
     @testset "1D: abs(x)" begin
       f = (x) -> abs(x[1])
-      g = x -> [(x[1] < 0) ? -1.0 : 1.0]
+      g = (x) -> [(x[1] < 0) ? -1.0 : 1.0]
 
       @testset "Minimise 1D" begin
         p = UnconstrainedNonSmoothProblem(f, g, 1, Minimise)
@@ -36,10 +36,47 @@ using Test
         @test length(x1) == 1
         @test x1[1] ≈ 12.0
       end
+
+      @testset "Maximise 1D (2)" begin
+        p = UnconstrainedNonSmoothProblem(f, g, 1, Maximise)
+        a = SubgradientMethod(ConstantStepSize(1.0), 2)
+
+        x0 = [-10.0]
+        x1 = solve(p, a, x0)
+        @test length(x1) == 1
+        @test x1[1] ≈ -12.0
+      end
+
+      @testset "Info callback" begin
+        cb_called = false
+        cb = (k, f, x, g, f_best, x_best, δt) -> begin
+          cb_called = true
+        end
+        p = UnconstrainedNonSmoothProblem(f, g, 1, Maximise)
+        a = SubgradientMethod(ConstantStepSize(1.0), 2)
+        solve(p, a, [0.0], info_callback=cb)
+
+        @test cb_called
+      end
     end
   end
   
   @testset "Projected subgradient" begin
+    @testset "1D: abs(x)" begin
+      f = (x) -> abs(x[1])
+      g = (x) -> [(x[1] < 0) ? -1.0 : 1.0]
+      proj = (x) -> [200.0]
+
+      @testset "Minimise 1D" begin
+        p = ProjectedConstrainedNonSmoothProblem(f, g, proj, 1, Minimise)
+        a = ProjectedSubgradientMethod(ConstantStepSize(1.0), 2)
+
+        x0 = [10.0] # Infeasible solution! Must be projected!
+        x1 = solve(p, a, x0)
+        @test length(x1) == 1
+        @test x1[1] ≈ 200.0
+      end
+    end
   end
   
   @testset "Bundle" begin
